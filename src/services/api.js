63b558c7acdfe48dc4698770d026dbdc664fa8e1
Om <omdwivedi234@gmail.com>
2025-04-14 15:15:31 +0530

    first commit

---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+
+const API_KEY = "9101b3232efb4d96c1a62fae542a6faa";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+	const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+	const data = await response.json();
+	return data.results;
+};
+
+export const SearchMovies = async () => {
+	const response = await fetch(
+		`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+			query
+		)}`
+	);
+	const data = await response.json();
+	return data.results;
+};
